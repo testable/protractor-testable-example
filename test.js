6f37b6c1fd1152914c5f098e0447f80041d15a22
Avi Stramer <astramer@gmail.com>
2023-12-21 21:56:50 -0600

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,19 @@
-const fs = require('fs');
+describe('angularjs homepage todo list', function() {
+  it('should add a todo', async function() {
+    browser.get('https://angularjs.org');
 
-describe('Protractor Demo App', function() {
-  it('should have a title', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    browser.takeScreenshot().then(function(base64Data) {
-      fs.writeFileSync((process.env.OUTPUT_DIR || '') + '/HomePage.png', base64Data, 'base64');
-    });
-    expect(browser.getTitle()).toEqual('Super Calculator');
+    element(by.model('todoList.todoText')).sendKeys('write first protractor test');
+    element(by.css('[value="add"]')).click();
+
+    var todoList = element.all(by.repeater('todo in todoList.todos'));
+    expect(todoList.count()).toEqual(3);
+    expect(todoList.get(2).getText()).toEqual('write first protractor test');
+
+    // You wrote your first test, cross it off the list
+    todoList.get(2).element(by.css('input')).click();
+    var completedAmount = element.all(by.css('.done-true'));
+    expect(completedAmount.count()).toEqual(2);
+
+    await browser.takeScreenshot();
   });
 });
